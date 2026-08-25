de7461d18ebf0decdcab3fde3ad958e563592e39
Hkshoonya <154622641+Hkshoonya@users.noreply.github.com>
2026-08-25 11:07:43 -0400

    Enforce HTTPS transport security

--- next.config.ts
+++ next.config.ts
@@ -49,6 +49,7 @@ const nextConfig: NextConfig = {
         source: "/(.*)",
         headers: [
           { key: "X-DNS-Prefetch-Control", value: "on" },
+          { key: "Strict-Transport-Security", value: "max-age=31536000" },
           { key: "X-Frame-Options", value: "DENY" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
